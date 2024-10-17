3f19c406cf4aad1eaa7a861f4b10a12126185997
abressiddique <abressiddique@gmail.com>
2024-10-17 23:54:01 +0530

    Update ProductCard.tsx

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -9,7 +9,7 @@ interface ProductCardProps {
   updateSignedInUser?: (updatedUser: UserType) => void;
 }
 
-const ProductCard = ({ product, updateSignedInUser }: ProductCardProps) => {
+const ProductCard = ({ product, updateSignedInUser }: ProductCardProps ) => {
   return (
     <Link
       href={`/products/${product._id}`}
@@ -27,7 +27,7 @@ const ProductCard = ({ product, updateSignedInUser }: ProductCardProps) => {
         <p className="text-small-medium text-grey-2">{product.category}</p>
       </div>
       <div className="flex justify-between items-center">
-        <p className="text-body-bold">₹{product.price}</p> {/* Changed here */}
+        <p className="text-body-bold">${product.price}</p>
         <HeartFavorite product={product} updateSignedInUser={updateSignedInUser} />
       </div>
     </Link>
