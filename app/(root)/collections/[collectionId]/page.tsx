72a3e2c7eebfb5f098d903e0d457e6ad124efac7
abressiddique <abressiddique@gmail.com>
2024-10-17 23:35:36 +0530

    Update page.tsx

--- app/(root)/collections/[collectionId]/page.tsx
+++ app/(root)/collections/[collectionId]/page.tsx
@@ -14,7 +14,7 @@ const CollectionDetails = async ({
     <div className="px-10 py-5 flex flex-col items-center gap-8">
       <Image
         src={collectionDetails.image}
-       width={1500}
+        width={1500}
         height={1000}
         alt="collection"
         className="w-full h-[400px] object-cover rounded-xl"
@@ -33,4 +33,3 @@ const CollectionDetails = async ({
 export default CollectionDetails;
 
 export const dynamic = "force-dynamic";
-
