77325f72bf804361089f3adcc066fa5e8cd2746f
abressiddique <abressiddique@gmail.com>
2024-10-18 22:02:51 +0530

    Update route.ts

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,36 +1,30 @@
 import User from "@/lib/models/User";
 import { connectToDB } from "@/lib/mongoDB";
 import { auth } from "@clerk/nextjs";
+
 import { NextRequest, NextResponse } from "next/server";
 
 export const GET = async (req: NextRequest) => {
   try {
-    const { userId } = auth();
-
-    // Log the retrieved userId for debugging
-    console.log("User ID:", userId);
+    const { userId } = auth()
 
     if (!userId) {
-      return new NextResponse(JSON.stringify({ message: "Unauthorized" }), { status: 401 });
+      return new NextResponse(JSON.stringify({ message: "Unauthorized" }), { status: 401 })
     }
 
-    await connectToDB();
+    await connectToDB()
 
-    let user = await User.findOne({ clerkId: userId });
+    let user = await User.findOne({ clerkId: userId })
 
-    // Log the retrieved user for debugging
-    console.log("Existing User:", user);
-
-    // When the user signs in for the first time, immediately create a new user for them
+    // When the user sign-in for the 1st, immediately we will create a new user for them
     if (!user) {
-      user = await User.create({ clerkId: userId });
-      await user.save();
-      console.log("New User Created:", user); // Log the new user creation
+      user = await User.create({ clerkId: userId })
+      await user.save()
     }
 
-    return NextResponse.json(user, { status: 200 });
+    return NextResponse.json(user, { status: 200 })
   } catch (err) {
-    console.log("[users_GET]", err);
-    return new NextResponse("Internal Server Error", { status: 500 });
+    console.log("[users_GET]", err)
+    return new NextResponse("Internal Server Error", { status: 500 })
   }
 }
